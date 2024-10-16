c7218c68333418392e7c5485f21f8a4d0dce5d29
heavendew <cielocalderon02@gmail.com>
2024-10-16 11:30:21 -0500

    herencia

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
+import React, { useState } from "react";
 import { TodoCounter } from './components/todoCounter/TodoCounter.js';
 import { TodoSearch } from './components/todoSearch/TodoSearch.js';
 import { TodoList } from './components/todoList/TodoList.js';
 import { TodoItem } from './components/todoItem/TodoItem.js';
-import {CreateTodoButton} from './components/createTodoButton/CreateTodoButton';
+import { CreateTodoButton } from './components/createTodoButton/CreateTodoButton';
 
 import './App.css';
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
   return (
     <>
-
-      <TodoCounter completed={18} total={23}/>
-      <TodoSearch />
-
-
+      <TodoCounter completed={18} total={23} />
+      <TodoSearch
+        searchValue={searchValue} handleChange={handleChange}
+        setSearchValue={setSearchValue} />
       <TodoList>
-        <TodoItem completed={true} text="Todo 1"/>
-        <TodoItem completed={false} text="Todo 2"/>
-        <TodoItem completed={false} text="Todo 3"/>
+        <TodoItem completed={true} text="Todo 1" />
+        <TodoItem completed={false} text="Todo 2" />
+        <TodoItem completed={false} text="Todo 3" />
       </TodoList>
 
       <CreateTodoButton />
+
+
+
+
+
+
 
     </>
   );
--- src/components/todoSearch/TodoSearch.js
+++ src/components/todoSearch/TodoSearch.js
@@ -1,16 +1,10 @@
-import React from "react";
+// import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState("hola");
-
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    setSearchValue(event.target.value);
-  };
-
-
-
+function TodoSearch(
+  {searchValue,
+  handleChange}
+) {
   return (
     <div>
       <input
@@ -20,7 +14,7 @@ function TodoSearch() {
         onChange={handleChange}
       />
 
-      <p>{searchValue}</p>
+      
     </div>
   );
 }
